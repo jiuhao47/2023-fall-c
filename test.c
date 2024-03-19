4efec54ce5793966928fcab7b8af32615fd16b66
Junyan Jiang <201499832@qq.com>
2024-03-19 10:24:16 +0800

    2024.3.19

--- test.c
+++ test.c
@@ -1,29 +1,23 @@
+#include <stdlib.h>
 #include <stdio.h>
-#define MAXSIZE 1000000
+#include <math.h>
+
+typedef struct
+{
+    int c;
+    int e;
+} Ployn;
+
+typedef struct
+{
+    Ployn *elem;
+    int length;
+} SqPloy;
+
+void create_SqPloy(SqPloy &a)
+{
+}
+
 int main()
 {
-    int num[MAXSIZE];
-    for (int i = 0; i < MAXSIZE; i++)
-    {
-        num[i] = 1;
-    }
-    for (int i = 2; i * i <= MAXSIZE; i++)
-    {
-        if (num[i])
-        {
-            for (int j = i + i; j < MAXSIZE; j += i)
-            {
-                num[j] = 0;
-            }
-        }
-    }
-
-    for (int i = 0; i < MAXSIZE; i++)
-    {
-        if (num[i])
-        {
-            printf("%d ", i);
-        }
-    }
-    return 0;
 }
